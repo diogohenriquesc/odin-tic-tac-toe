a5593138d058ac9e9de371205bee6bdbf0c0da58
Diogo Henrique <diogohs110@gmail.com>
2023-08-06 20:21:56 -0300

    Add ui functionality

--- js/script.js
+++ js/script.js
@@ -6,46 +6,62 @@ const gameboard = (() => {
 
 	const addMarker = (position, value) => (_board[position] = value);
 
-	const getIndexes = (marker) => {
-		return _board
-			.reduce((indexes, currentElement, currentIndex) => {
-				if (currentElement === marker) {
-					indexes.push(currentIndex);
-				}
-				return indexes;
-			}, [])
-			.join('');
+	const resetBoard = () => {
+		_board = new Array(9);
 	};
 
+	const hasEmptySlots = () => {
+		return _board.includes(undefined);
+	};
 	return {
 		getBoard,
 		addMarker,
-		getIndexes,
+		resetBoard,
+		hasEmptySlots,
 	};
 })();
 
 // Player Factory
-const createPlayer = (name, marker, markerIcon) => {
+function createPlayer(name, marker) {
+	let points = 0;
+
 	return {
 		name,
 		marker,
-		markerIcon,
+		points,
+		changeName: function (newName) {
+			this.name = newName;
+		},
+		addPoint: function () {
+			this.points += 1;
+		},
+		resetPoints: function () {
+			this.points = 0;
+		},
 	};
-};
+}
 
-/* The gameConstroler will be responsible for controlling the flow and state of the games's turn, as well as 
+/* The gameConstroler will be responsible for controlling the flow and state of the games's, as well as 
 whether anybody has won the game */
 const gameControler = (() => {
 	const _players = [
-		createPlayer('Player One', 'x', '<i class="fa-solid fa-x"></i>'),
-		createPlayer('Player Two', 'o', '<i class="fa-regular fa-circle"></i>'),
+		createPlayer('Player One', 'x'),
+		createPlayer('Player Two', 'o'),
 	];
 
 	let _currentPlayer = _players[0];
 
+	const getPlayers = () => _players;
+
 	const getCurrentPlayer = () => _currentPlayer;
 
-	const getPlayers = () => _players;
+	const changePlayerName = (playerId, newName) => {
+		_players[playerId].changeName(newName);
+	};
+
+	const getPlayerPoints = (playerId) => {
+		return _players[playerId].points;
+	};
 
 	const _switchTurns = () => {
 		_currentPlayer = _currentPlayer === _players[0] ? _players[1] : _players[0];
@@ -62,79 +78,161 @@ const gameControler = (() => {
 		'246',
 	];
 
+	const _getPlayerPositions = (marker) => {
+		const board = gameboard.getBoard();
+
+		const playerIndexes =  board.reduce((indexes, position, index) => {
+			if (position === marker) {
+				indexes.push(index);
+			}
+
+			return indexes;
+		}, []);
+
+		return playerIndexes.join('');
+	} 
 
 	const _checkForWinner = () => {
-		const currentPlayerMoves = gameboard.getIndexes(_currentPlayer.marker);
-		if (currentPlayerMoves.length < 3) return;
+		const currentPlayerPositions = _getPlayerPositions(_currentPlayer.marker);
+		if (currentPlayerPositions.length < 3) return;
 
-		for (let i = 0; i < _winningPositions.length; i++) { 
-      if (
-				checkString(currentPlayerMoves, _winningPositions[i][0]) &&
-				checkString(currentPlayerMoves, _winningPositions[i][1]) &&
-				checkString(currentPlayerMoves, _winningPositions[i][2])
+		for (let i = 0; i < _winningPositions.length; i++) {
+			if (
+				_checkPositions(currentPlayerPositions, _winningPositions[i][0]) &&
+				_checkPositions(currentPlayerPositions, _winningPositions[i][1]) &&
+				_checkPositions(currentPlayerPositions, _winningPositions[i][2])
 			)
-				return true;
+				return {
+					message: true,
+					position: _winningPositions[i],
+				};
 		}
 	};
 
-	const checkString = (string, value) => {
-		return string.includes(value) ? true : false;
-  } 
+	const _checkPositions = (playerPositions, value) => {
+		return playerPositions.includes(value) ? true : false;
+	};
+
+	const _checkForTie = () => {
+		return !gameboard.hasEmptySlots();
+	};
 
 	const playRound = (position) => {
-		if (!gameboard.getBoard()[position]) {
+		if (!gameboard.getBoard()[position] && !_checkForWinner()) {
 			gameboard.addMarker(position, _currentPlayer.marker);
 
 			if (_checkForWinner()) {
-        document.querySelector('.message p').textContent = `${_currentPlayer.name} is the winner!`;
+				_players[_currentPlayer.marker === 'x' ? 0 : 1].addPoint();
+				return 'winner';
+			} else if (_checkForTie()) {
+				return 'tie';
 			} else {
-        _switchTurns();
-      }
+				_switchTurns();
+				return null;
+			}
 		}
+	};
+
+	const resetGame = () => {
+		gameboard.resetBoard();
+		_currentPlayer = _players[0];
 	};
 
 	return {
 		getPlayers,
 		getCurrentPlayer,
 		playRound,
+		resetGame,
+		changePlayerName,
+		getPlayerPoints,
 	};
 })();
 
 // The display controler will interact with the DOM
 const displayControler = (() => {
-	const board = gameboard.getBoard();
-	const boardCells = document.querySelectorAll('.gameboard__cell');
+	const _header = document.querySelector('.header');
+	const _boardCells = document.querySelectorAll('.gameboard__cell');
+	const _nameInputs = document.querySelectorAll('.player__info-name');
+	const _replayButton = document.querySelector('.btn-replay');
+	const _messageDisplay = document.querySelector('.header__message p');
+	const _scoreDisplays = document.querySelectorAll('.player__info-score');
 
-	boardCells.forEach((cell) =>
+	_boardCells.forEach((cell) =>
 		cell.addEventListener('click', (e) => {
-			cellClick(e);
+			_cellClick(e);
 		})
 	);
 
-	const renderBoard = () => {
+	_nameInputs.forEach((input) => {
+		input.addEventListener('change', (e) => _inputChange(e));
+	});
+
+	_replayButton.addEventListener('click', () => _replayClick());
+
+	const _cellClick = (e) => {
+		const clickedCell = e.target.dataset.number;
+		const gameStatus = gameControler.playRound(clickedCell);
+		_updateBoard();
+		_updateHeader();
+
+		if (gameStatus === 'winner') {
+			_displayMessage(`${gameControler.getCurrentPlayer().name} is the winner!`);
+		} else if (gameStatus === 'tie') {
+			_displayMessage(`It's a tie!`);
+		}
+	};
+
+	const _inputChange = (e) => {
+		const input = e.target;
+		const playerId = Number(input.closest('.player__info').dataset.player);
+		let newName = input.value;
+
+		if (newName === '') {
+			newName = playerId === 0 ? 'Player One' : 'Player Two';
+			input.value = newName;
+		}
+
+		gameControler.changePlayerName(playerId, newName);
+	}
+
+	const _replayClick = () => {
+		gameControler.resetGame();
+		_updateBoard();
+		_updateHeader();
+		_displayMessage('');
+	}
+
+	const _displayMessage = (message) => {
+		_messageDisplay.textContent = message;
+	}
+
+	const _renderBoard = () => {
+		const board = gameboard.getBoard();
+
 		for (let i = 0; i < board.length; i++) {
 			if (board[i] === 'x' || board[i] === 'o') {
-				boardCells[i].innerHTML = gameControler
-					.getPlayers()
-					.find((elm) => elm.marker === board[i]).markerIcon;
+				_boardCells[i].textContent = board[i];
 			}
 		}
 	};
 
-	const clearBoard = () => {
-		boardCells.forEach((cell) => {
-			cell.innerHTML = '';
+	const _clearBoard = () => {
+		_boardCells.forEach((cell) => {
+			cell.textContent = '';
 		});
 	};
 
-	const updateBoard = () => {
-		clearBoard();
-		renderBoard();
+	const _updateBoard = () => {
+		_clearBoard();
+		_renderBoard();
 	};
 
-	const cellClick = (e) => {
-		const clickedCell = e.target.dataset.number;
-		gameControler.playRound(clickedCell);
-		updateBoard();
+	const _updateHeader = () => {
+		const currentPlayer = gameControler.getCurrentPlayer();
+
+		_header.setAttribute('data-selected', currentPlayer.marker === 'x' ? 0 : 1);
+		_scoreDisplays.forEach((display, index) =>
+				(display.textContent = gameControler.getPlayerPoints(index))
+		);
 	};
 })();
